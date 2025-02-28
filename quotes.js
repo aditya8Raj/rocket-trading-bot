3cc1aa963a3fc64aa9cfbdafbef7177caef337ee
Aditya Raj <aditya7raj1@gmail.com>
2025-02-28 15:19:26 +0530

    new quotes added

--- quotes.js
+++ quotes.js
@@ -1,16 +1,19 @@
 const tradingWisdom = [
-  "Patience is a trader's best friend.",
-  "The trend is your friend until it ends.",
+  "All you need is one pattern to make a living.",
+  "Trading doesn’t just reveal your character; it also builds it if you stay in the game long enough.",
+  "Every trader has strengths and weaknesses. Some are good holders of winners but may hold their losers a little too long. Others may cut their winners a little short but are quick to take their losses. As long as you stick to your own style, you get the good and bad in your own approach.",
   "Never risk more than you can afford to lose.",
-  "Successful trading is about managing risk, not avoiding it.",
+  "Trading is very competitive and you have to be able to handle getting your butt kicked.",
+  "It’s not whether you’re right or wrong that’s important, it’s how much money you make when you’re right and how much you lose when you’re wrong.",
+  "Do not anticipate and move without market confirmation - being a little late in your trade is your insurance that you are right or wrong.",
   "The stock market is a device for transferring money from the impatient to the patient.",
+  "Don’t worry about what the markets are going to do, worry about what you are going to do in response to the markets.",
   "Do not be a hero. Be a smart trader.",
-  "The goal of a successful trader is to make the best trades. Money is secondary.",
-  "Adapt to the market; do not expect the market to adapt to you.",
-  "Focus on the process, not the outcome.",
-  "Learn from your mistakes and move on.",
-  "Discipline is the bridge between goals and accomplishment.",
-  "In trading, less is often more.",
+  "An investment in knowledge pays the best interest.",
+  "Patterns don't work 100% of the time. But they are still critical because they help you define your risk. If you ignore patterns and focus on hunches, feelings, and hot tips, just forget about achieving consistency.",
+  "You have to identify your weaknesses and work to change. Keep a trading diary – write down your reasons for entering and exiting every trade. Look for repetitive patterns of success and failure.",
+  "The market is a device for transferring money from the impatient to the patient.",
+  "Hope is not a strategy.",
 ];
 
 module.exports = tradingWisdom;
